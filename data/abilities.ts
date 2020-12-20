f28739a2b10824b0398ed597b2ceb1d24346628e
Poilerwags <giestheuws@gmail.com>
2020-12-20 17:59:49 +0100

    Can't overwrite regurg attack name, that's fine

--- data/abilities.ts
+++ data/abilities.ts
@@ -3589,7 +3589,6 @@ export const Abilities: {[abilityid: string]: AbilityData} = {
 			const regurgMove = this.dex.getActiveMove('tackle');
 			regurgMove.category = move.category;
 			regurgMove.accuracy = true;
-			regurgMove.name = "Regurgitation";
 			if (source.species.id === 'mukdeltawater') regurgMove.type = 'Water';
 			if (source.species.id === 'mukdeltagrass') regurgMove.type = 'Grass';
 			if (source.species.id === 'mukdeltafire') regurgMove.type = 'Fire';
