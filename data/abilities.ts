5b90c6f6d8a26f338d9165811893456766e07cc2
Poilerwags <giestheuws@gmail.com>
2020-12-22 18:32:34 +0100

    this is dumb

--- data/abilities.ts
+++ data/abilities.ts
@@ -3666,35 +3666,19 @@ export const Abilities: {[abilityid: string]: AbilityData} = {
 		onAfterMoveSecondarySelf(pokemon, target, move) {
 			if (move.category === 'Status') return;
 			if (pokemon.species.baseSpecies !== 'Muk-Delta') return;
-			if (move.flags['contact']) {
-				const regurgMove = this.dex.getActiveMove('tackle');
-				regurgMove.category = move.category;
-				regurgMove.accuracy = true;
-				regurgMove.name = "Regurgitation";
-				if (pokemon.species.id === 'mukdeltawater') regurgMove.type = 'Water';
-				if (pokemon.species.id === 'mukdeltagrass') regurgMove.type = 'Grass';
-				if (pokemon.species.id === 'mukdeltafire') regurgMove.type = 'Fire';
-				if (pokemon.species.id === 'mukdeltadark') regurgMove.type = 'Dark';
-				if (pokemon.species.id === 'mukdeltanormal') regurgMove.type = 'Normal';
-				if (pokemon.species.id === 'mukdeltapsychic') regurgMove.type = 'Psychic';
-				if (move.name === "Regurgitation") return;
-				this.useMove(regurgMove, pokemon, target);
-				return null;
-			} else {
-				const regurgMove = this.dex.getActiveMove('fairywind');
-				regurgMove.category = move.category;
-				regurgMove.accuracy = true;
-				regurgMove.name = "Regurgitation";
-				if (pokemon.species.id === 'mukdeltawater') regurgMove.type = 'Water';
-				if (pokemon.species.id === 'mukdeltagrass') regurgMove.type = 'Grass';
-				if (pokemon.species.id === 'mukdeltafire') regurgMove.type = 'Fire';
-				if (pokemon.species.id === 'mukdeltadark') regurgMove.type = 'Dark';
-				if (pokemon.species.id === 'mukdeltanormal') regurgMove.type = 'Normal';
-				if (pokemon.species.id === 'mukdeltapsychic') regurgMove.type = 'Psychic';
-				if (move.name === "Regurgitation") return;
-				this.useMove(regurgMove, pokemon, target);
-				return null;
-			}
+			const regurgMove = this.dex.getActiveMove('tackle');
+			regurgMove.category = move.category;
+			regurgMove.accuracy = true;
+			regurgMove.name = "Regurgitation";
+			if (pokemon.species.id === 'mukdeltawater') regurgMove.type = 'Water';
+			if (pokemon.species.id === 'mukdeltagrass') regurgMove.type = 'Grass';
+			if (pokemon.species.id === 'mukdeltafire') regurgMove.type = 'Fire';
+			if (pokemon.species.id === 'mukdeltadark') regurgMove.type = 'Dark';
+			if (pokemon.species.id === 'mukdeltanormal') regurgMove.type = 'Normal';
+			if (pokemon.species.id === 'mukdeltapsychic') regurgMove.type = 'Psychic';
+			if (move.name === "Regurgitation") return;
+			this.useMove(regurgMove, pokemon, target);
+			return null;
 		},
 		name: "Regurgitation",
 		rating: 3,
